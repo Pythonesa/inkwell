3d8bf521f8d9a636617bcf9c5849da9bae2da39d
Pythonesa <pythonesamar@gmail.com>
2024-07-31 02:53:54 -0300

    :lipstick: (front): base nav

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'dark-background': '#1f1f1f',
+        'light-background': '#fff6e5',
+        'primary': '#212121',
+        'shadow': '#6c6c6b',
+        'highlight': '#fcf2e3',
+      },
+    },
+  },
+  plugins: [],
+}
