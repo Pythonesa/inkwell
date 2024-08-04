80c4306f2f94183d740973a869f8d4e4da55f70b
Pythonesa <pythonesamar@gmail.com>
2024-08-04 16:16:24 -0300

    ✨(users): add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    phone_number = models.CharField(_("número de teléfono"), max_length=15)
+
+    email = models.EmailField(_("email address"), null=True, blank=True)
+    birth_date = models.DateField(_("fecha de nacimiento"), null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
